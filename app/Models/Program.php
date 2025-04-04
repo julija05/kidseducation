f02d298af91af9813413063d14f41db8fcbb8cf1
julija05 <julijadelik@yahoo.com>
2025-04-04 11:22:47 +0200

    KE-24 Created Model for Student

--- app/Models/Program.php
+++ app/Models/Program.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Program extends Model
 {
@@ -21,4 +22,9 @@ class Program extends Model
         'duration',
         'price',
     ];
+
+    public function students(): BelongsToMany
+    {
+        return $this->belongsToMany(Student::class, 'student_program');
+    }
 }
