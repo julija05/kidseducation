76e55df1d07b0e8b509580e0052afc0fcff77ac1
julija05 <julijadelik@yahoo.com>
2025-04-05 11:55:55 +0200

    Last Update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    build: {
+        outDir: "public/build",
+        manifest: true,
+        emptyOutDir: true,
+    },
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: "resources/js/app.jsx",
             refresh: true,
         }),
         react(),
