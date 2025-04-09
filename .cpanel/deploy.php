900cb953a36907aa6cf31bbdcce4ba5e34470dfe
julija05 <julijadelik@yahoo.com>
2025-04-09 17:07:40 +0200

    Adding scripts

---
+++ .cpanel/deploy.php
@@ -0,0 +1,34 @@
+<?php
+$username = 'your_cpanel_username';
+$deployPath = '/home/' . $username . '/public_html/';
+$appPath = '/home/' . $username . '/repositories/kidseducation/';
+
+// Copy public files
+shell_exec("/bin/cp -R {$appPath}public/* {$deployPath}");
+
+// Copy application files one directory up from public_html
+$appDestination = dirname($deployPath);
+shell_exec("/bin/cp -R {$appPath}app {$appDestination}/");
+shell_exec("/bin/cp -R {$appPath}bootstrap {$appDestination}/");
+shell_exec("/bin/cp -R {$appPath}config {$appDestination}/");
+shell_exec("/bin/cp -R {$appPath}database {$appDestination}/");
+shell_exec("/bin/cp -R {$appPath}resources {$appDestination}/");
+shell_exec("/bin/cp -R {$appPath}routes {$appDestination}/");
+shell_exec("/bin/cp -R {$appPath}storage {$appDestination}/");
+shell_exec("/bin/cp {$appPath}.env {$appDestination}/");
+shell_exec("/bin/cp {$appPath}artisan {$appDestination}/");
+shell_exec("/bin/cp {$appPath}composer.json {$appDestination}/");
+shell_exec("/bin/cp {$appPath}composer.lock {$appDestination}/");
+
+// Run necessary commands
+chdir($appDestination);
+shell_exec("/usr/local/bin/php /opt/cpanel/composer/bin/composer install --no-dev");
+shell_exec("/usr/local/bin/php artisan migrate --force");
+shell_exec("/usr/local/bin/php artisan cache:clear");
+shell_exec("/usr/local/bin/php artisan config:clear");
+shell_exec("/usr/local/bin/php artisan view:clear");
+shell_exec("/usr/local/bin/php artisan route:clear");
+shell_exec("/usr/local/bin/npm install");
+shell_exec("/usr/local/bin/npm run build");
+
+echo "Deployment completed.\n";
