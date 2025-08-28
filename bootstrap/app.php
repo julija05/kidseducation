2db1ff96a616eed6dc3aa56ba5063065b0b54678
julija05 <julijadelik@yahoo.com>
2025-08-28 20:15:22 +0200

    Last Update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,7 +17,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
-            \App\Http\Middleware\EnsureSessionConfiguration::class,
             \App\Http\Middleware\SetLocale::class,
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
