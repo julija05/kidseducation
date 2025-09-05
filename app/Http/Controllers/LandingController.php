965cd6a1ebe4ec4606c487ce8956a883cde8da4d
julija05 <julijadelik@yahoo.com>
2025-09-05 13:35:29 +0200

    Last Update

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -2,22 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\News;
 use Illuminate\Support\Facades\Route;
 
 class LandingController extends Controller
 {
     public function index()
     {
-        $articles = News::published()
-            ->latest('published_at')
-            ->take(6)
-            ->get();
-
-
         return $this->createView('Front/Home/Home', [
             'canLogin' => Route::has('login'),
-            'articles' => $articles,
             // 'canRegister' => Route::has('register'),
         ]);
     }
